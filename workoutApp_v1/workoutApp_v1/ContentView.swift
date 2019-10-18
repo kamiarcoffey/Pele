6b9c785161da3c0ebb749461e06cfc85d38d2ae8
Kamiar Coffey <kamiar.coffey@gmail.com>
2019-10-18 08:07:29 -0600

    refactor to be a workspace for CocoaPods dependencies

---
+++ workoutApp_v1/workoutApp_v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  workoutApp_v1
+//
+//  Created by Kamiar Coffey on 10/18/19.
+//  Copyright © 2019 Kamiar Coffey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ workoutApp_v1/workoutApp_v1/Models/PlaylistModel.swift
@@ -0,0 +1,49 @@
+//
+//  PlayListModel.swift
+//  workoutApp
+//
+//  Created by Kamiar Coffey on 10/17/19.
+//  Copyright © 2019 Kamiar Coffey. All rights reserved.
+//
+
+import Foundation
+
+class WorkoutPlaylist {
+    
+    private var playList = [String: [Exercise]]
+    
+    init(with startingPlaylist: [String: [Exercise]]) {
+        self.playList = startingPlaylist
+    }
+    
+    private func getAllExercices() -> [Exercise] {
+        return playList.values.flatMap{ $0.map{$0}}
+    }
+    
+    private func getAllExerciceNames() -> [String] {
+        return playList.values.flatMap{ $0.map{$0.getName()}}
+    }
+
+    private func populateWilcard() {
+        let name = "All Exercises"
+        let routines = getAllExercices()
+        self.playList[name] = routines
+    }
+    
+    // allows for passing in just the name
+    func addPlaylist(with name: String, having routine: [Exercise] = [Exercise]()) {
+        self.playList[name] = routine
+    }
+    
+    
+    func getPlaylist() {
+        populateWilcard()
+        return self.playList
+    }
+    
+    func getPlaylistNames() {
+        populateWilcard()
+        return playList.keys.sort()
+    }
+    
+}
