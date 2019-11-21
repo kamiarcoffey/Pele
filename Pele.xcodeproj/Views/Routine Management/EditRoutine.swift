050b6179d0df1656ee7c602963ba47de1cddb2b5
Kamiar Coffey <kamiar.coffey@gmail.com>
2019-11-21 14:19:00 -0700

    2nd backup

--- Pele.xcodeproj/Views/Routine Management/EditRoutine.swift
+++ Pele.xcodeproj/Views/Routine Management/EditRoutine.swift
@@ -15,14 +15,30 @@ struct EditRoutine: View {
     @State var routineBeingEdited: PeleRoutine
     @State var isPresentingAddExercise = false
     @Binding var isPresenting: Bool
-    @State var exerciseManager = ExercisePoolManager()
-    @ObservedObject var playlists = PlaylistsManager.shared
+    @State var exercises = ExercisePoolManager()
     @State var selectedExercises: [PeleExercise] = []
     
     mutating func addToSelected(new exercise: PeleExercise) {
         self.selectedExercises.append(exercise)
     }
-
+    
+    struct Row: Identifiable {
+        let id = UUID()
+        let cells: [PeleExercise]
+    }
+    
+    
+    // some gross code to make a 2 column grid list quickly
+    func formatTwoColumMatrix(flatList: [PeleExercise]) -> [Row] {
+        var retList = [Row]()
+        for i in 0..<flatList.count where i % 2 == 0 { // even numbers only
+            retList.append(Row(cells: [flatList[i], flatList[i+1]]))
+        }
+        return retList
+    }
+    
+    /* end viewController */
+    
     var body: some View {
         VStack {
             NavigationView {
@@ -30,41 +46,54 @@ struct EditRoutine: View {
                     Text(routineBeingEdited.name)
                     Divider()
                     
+                    List {
+                        ForEach(exercises.getExercisePool.filter({self.routineBeingEdited.containsExercise($0)}), id: \.self) { thisExercise in
+                            ExerciseRow(exercise: thisExercise)
+                        }
+                        .onDelete(perform: exercises.deleteItem)
+                        .onMove(perform: exercises.move)
+                    }
+                    
                     VStack {
-                        ForEach(self.exerciseManager.getExercisePool) { thisExercise in
+                        ForEach(self.exercises.getExercisePool) { thisExercise in
                             BoxView(exercise: thisExercise, isSelected: self.selectedExercises.contains(thisExercise)) {
                                 if self.selectedExercises.contains(thisExercise) {
                                     self.selectedExercises.removeAll(where: { $0 == thisExercise })
-                                    self.playlists.removeExercise(from: self.routineBeingEdited.getName, having: thisExercise.getName())
                                 }
                                 else {
                                     self.selectedExercises.append(thisExercise)
-                                    self.playlists.addExercise(from: self.routineBeingEdited.getName, having: thisExercise)
                                 }
                             }
                         }
                     }
                     .padding(EdgeInsets.init(top: +10, leading: +10, bottom: +10, trailing: +10))
-                    
+
                 }
-                .navigationBarTitle(Text(routineBeingEdited.name))
+                .navigationBarTitle(Text("Your Exercises"))
                 .navigationBarItems(leading: Button(action: {
                     self.isPresentingAddExercise.toggle()
                 }, label: {
                     Text("Create New Exercise")
-                        .foregroundColor(.orange)
-                    }),
-                        trailing: Button(action: {
-                            self.exerciseManager.deleteExercises(exercises: self.selectedExercises)
-                            self.isPresenting.toggle()
-                        }, label: {
-                            Image(systemName: "trash")
-                        }))
+                        .foregroundColor(.white)
+                        .padding(.all, 6)
+                        .background(Color(red: 0.67, green: 0.6, blue:0.0 ))
+                }),
+                                    trailing: Button(action: {
+                                        self.routineBeingEdited.addExercises(self.selectedExercises)
+                                        self.isPresenting.toggle()
+                                    }, label: {
+                                        Text("Add Selected Exercises")
+                                            .foregroundColor(.white)
+                                            .padding(.all, 6)
+                                            .background(Color(red: 1.67, green: 0.6, blue:0.0, opacity: 1.0 ))
+                                    }))
+                    
                     .sheet(isPresented: $isPresentingAddExercise, content: {
                         AddExercise(isPresenting: self.$isPresentingAddExercise, didCreateExercise: { exercise in
-                            self.exerciseManager.addExercise(exercise: exercise)
+                            self.exercises.addExercise(exercise: exercise)
                         })
                     })
+                
                 
             }
         }.font(.system(size: 15))
