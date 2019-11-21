978287485264288fc3625c76847c1fe9e56c2433
Kamiar Coffey <kamiar.coffey@gmail.com>
2019-11-21 11:31:04 -0700

    alerts added, functionality for viewing historical sets during present logging added, name changed

---
+++ Pele/Pele.xcodeproj/ViewModels/LogExerciseViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  ExerciseViewModel.swift
+//  workoutApp_v1
+//
+//  Created by Kamiar Coffey on 11/7/19.
+//  Copyright © 2019 Kamiar Coffey. All rights reserved.
+//
+
+import Foundation
+
+class LogExerciseViewModel: ObservableObject {
+    
+    // this will belong to a specific exercise
+    // it can have an undetermined number of SetViewModel (one for each set of that exercise)
+    @Published var setsInProgress: [SetViewModel]
+    @Published var completedSets: [PeleExerciseSet]
+    private var exercise: PeleExercise
+    
+    var id = UUID()
+    
+    init(exercise: PeleExercise) {
+        self.exercise = exercise
+        setsInProgress = []
+        completedSets = []
+        self.generateAdditionalSet()
+        self.generateAdditionalSet()
+        self.generateAdditionalSet()
+    }
+    
+    func generateAdditionalSet() {
+        setsInProgress.append(SetViewModel(exercise: self.exercise))
+    }
+    
+    func logSet(finishedSet: PeleExerciseSet) {
+        completedSets.append(finishedSet)
+        
+    }
+    
+    public func displayName() -> String {
+        return self.exercise.getName()
+    }
+    
+    public func getExerciseDetails() -> (String, UUID, MuscleGroup) {
+        return (exercise.name, exercise.id, exercise.targetMuscle)
+    }
+}
+
